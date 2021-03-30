e92604d2ccf562f35f1490bd01584a1221d620db
Kolycheva Anna <annakolyt@gmail.com>
2021-03-30 13:41:56 +0700

    added test

---
+++ README.md
@@ -0,0 +1,2 @@
+## Unit-тестирование
+[![Build status](https://ci.appveyor.com/api/projects/status/2g69rsfcsp7t4koc/branch/master?svg=true)](https://ci.appveyor.com/project/Anna-Kolycheva/ajs-test-ci-2/branch/master)
--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,14 +1,25 @@
 import heroesSort from '../app';
 
-test('case 1', () => {
-  const result = heroesSort([
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ]);
-  expect(result).toEqual([
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ]);
+const heroes = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+const sortedHeroes = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+const result = heroesSort(heroes);
+
+test('toEqual', () => {
+  expect(result).toEqual(sortedHeroes);
+});
+
+test('toBe', () => {
+  expect(JSON.stringify(result)).toBe(JSON.stringify(sortedHeroes));
+});
+
+test('not.toBe', () => {
+  expect(result).not.toBe(sortedHeroes);
 });
